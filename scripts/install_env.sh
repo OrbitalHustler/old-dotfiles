c18eb2bc436c305d6ee72743602a891dac836cda
Mathieu Merino <mmerino@outlook.fr>
2021-10-12 01:23:45 +0200

    update

--- scripts/install_env.sh
+++ scripts/install_env.sh
@@ -5,13 +5,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 . "$DIR/base.sh"
 cd $DIR
 
-TMUX_TPM_PATH="$HOME/.tmux/plugins/tpm"
-if [ ! -d "$TMUX_TPM_PATH" ]; then
-    mkdir -p "$TMUX_TPM_PATH"
-    git clone https://github.com/tmux-plugin/tpm "$TMUX_TPM_PATH"
-    ~/.tmux/plugins/tpm/bin/install_plugins
-fi
-
 VIM_PLUG_PATH="$HOME/.vim/autoload"
 if [ ! -d "$VIM_PLUG_PATH" ]; then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \

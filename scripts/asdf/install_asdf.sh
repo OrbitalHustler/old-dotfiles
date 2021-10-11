73440f31277b99799fe2899059ad9de7542636df
Mathieu Merino <mmerino@outlook.fr>
2021-10-12 01:07:09 +0200

    update

--- scripts/asdf/install_asdf.sh
+++ scripts/asdf/install_asdf.sh
@@ -3,6 +3,8 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 . "$DIR/../base.sh"
 
+ASDF_DIR=${ASDF_DIR:="$HOME/.local/asdf"}
+
 if [ ! -d "$ASDF_DIR" ]; then
     git clone https://github.com/asdf-vm/asdf.git "$ASDF_DIR" --branch v0.8.1
     . ~/.local/asdf/asdf.sh

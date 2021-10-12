0d801443a8b967313f91ce70586fa4807efd2a49
Mathieu Merino <mmerino@outlook.fr>
2021-10-12 02:11:37 +0200

    update

--- containerfiles/deb11/chezmoi-init.sh
+++ containerfiles/deb11/chezmoi-init.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 BINDIR="$HOME/.local/bin" sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply Percee
+cd "$HOME/.local/share/chezmoi"
--- containerfiles/deb11/podman-attach.sh
+++ containerfiles/deb11/podman-attach.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 cd "$DIR"
 
-podman run --name deb11 -ti deb11 zsh
-# podman run --name deb11 -ti deb11 bash
+podman start -ia deb11

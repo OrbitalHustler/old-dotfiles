168eaa8f3c4979f777d0ea90469897b36ce324d8
Mathieu Merino <mmerino@outlook.fr>
2021-10-20 02:12:53 +0200

    update

--- containerfiles/deb11/chezmoi-init.sh
+++ containerfiles/deb11/chezmoi-init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-BINDIR="$HOME/.local/bin" sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply Percee
+BINDIR="$HOME/.local/bin" sh -c "$(curl -fsLS git.io/chezmoi)" -- init Percee
 cd "$HOME/.local/share/chezmoi"

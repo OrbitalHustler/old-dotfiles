53cab3f9afda293964f94b269cca660ca33db514
Mathieu Merino <mmerino@outlook.fr>
2021-12-17 10:20:17 +0100

    up

--- containerfiles/deb11/emacs/clone-emacs.sh
+++ containerfiles/deb11/emacs/clone-emacs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-git clone --depth 1 https://git.savannah.gnu.org/git/emacs.git
+git clone --depth 1 --branch emacs-28.0.90 https://git.savannah.gnu.org/git/emacs.git
